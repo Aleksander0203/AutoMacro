e16226bb71ba503033b1ee81452ecb2d06b87708
Aleksander <alexzace05@outlook.com>
2025-07-18 17:44:26 +0100

    Changed MouseHandler to include keyboard input handling. Added a function to save inputs to a file. Added an enum to represent each type of input event

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,135 @@
 #include <iostream>
+#include <fstream>
 #include <thread>
 #include <chrono>
-#include <ctime>
 #include <list>
 #include <X11/Xlib.h>
 #include <unistd.h>
 #include <X11/extensions/XInput2.h>
 #include <X11/extensions/XTest.h>
 #include <X11/keysym.h>
+#include <X11/XKBlib.h>
 #include <format>
 
+namespace Keys{
 
-class MouseHandler {
+            enum KEYSFORKEYCODE {
+                //Letters
+                A = XK_A,
+                B = XK_B,
+                C = XK_C,
+                D = XK_D,
+                E = XK_E,
+                F = XK_F,
+                G = XK_G,
+                H = XK_H,
+                I = XK_I,
+                J = XK_J,
+                K = XK_K,
+                L = XK_L,
+                M = XK_M,
+                N = XK_N,
+                O = XK_O,
+                P = XK_P,
+                Q = XK_Q,
+                R = XK_R,
+                S = XK_S,
+                T = XK_T,
+                U = XK_U,
+                V = XK_V,
+                W = XK_W,
+                X = XK_X,
+                Y = XK_Y,
+                Z = XK_Z,
 
-    private:
+                //Nums
+                KEY_0 = XK_0,
+                KEY_1 = XK_1,
+                KEY_2 = XK_2,
+                KEY_3 = XK_3,
+                KEY_4 = XK_4,
+                KEY_5 = XK_5,
+                KEY_6 = XK_6,
+                KEY_7 = XK_7,
+                KEY_8 = XK_8,
+                KEY_9 = XK_9,
 
-        int x;
-        int y;
-        Display* display;
-        bool pressed;
+                //Fn
+                F1  = XK_F1,
+                F2  = XK_F2,
+                F3  = XK_F3,
+                F4  = XK_F4,
+                F5  = XK_F5,
+                F6  = XK_F6,
+                F7  = XK_F7,
+                F8  = XK_F8,
+                F9  = XK_F9,
+                F10 = XK_F10,
+                F11 = XK_F11,
+                F12 = XK_F12,
+
+                //modifiers
+                Shift_L = XK_Shift_L,
+                Shift_R = XK_Shift_R,
+                Control_L = XK_Control_L,
+                Control_R = XK_Control_R,
+                Alt_L = XK_Alt_L,
+                Alt_R = XK_Alt_R,
+                Super_L = XK_Super_L,
+                Super_R = XK_Super_R,
+                Meta_L = XK_Meta_L,
+                Meta_R = XK_Meta_R,
+
+                //Special keys
+                Escape = XK_Escape,
+                Tab = XK_Tab,
+                Return = XK_Return,
+                Backspace = XK_BackSpace,
+                Space = XK_space,
+                Insert = XK_Insert,
+                Delete = XK_Delete,
+                Home = XK_Home,
+                End = XK_End,
+                Page_Up = XK_Page_Up,
+                Page_Down = XK_Page_Down,
+                Left = XK_Left,
+                Right = XK_Right,
+                Up = XK_Up,
+                Down = XK_Down,
+                Caps_Lock = XK_Caps_Lock,
+                Num_Lock = XK_Num_Lock,
+                Scroll_Lock = XK_Scroll_Lock,
+                Minus = XK_minus,
+                Equal = XK_equal,
+                Bracket_Left = XK_bracketleft,
+                Bracket_Right = XK_bracketright,
+                Semicolon = XK_semicolon,
+                Apostrophe = XK_apostrophe,
+                Grave = XK_grave,
+                Backslash = XK_backslash,
+                Comma = XK_comma,
+                Period = XK_period,
+                Slash = XK_slash,
+            };
+        }
+
+    namespace event {
+        
+        enum EVENTTYPE {
+            MouseMotion,
+            MousePressed,
+            MouseReleased,
+            KeyPressed,
+            keyReleased,
+        };
+
+    }
+
+class InputHandler {
 
     public: 
 
-        MouseHandler(Display *display) {
+        InputHandler(Display* display) {
             this->display = display;
             setMouseCoordinates();    
         }
@@ -71,7 +178,7 @@ class MouseHandler {
             this->pressed = pressed;
         }
         
-        void recordMouseEvent() {
+        void recordInputEvent() {
             Display* display = this->getDisplay();
             XIEventMask eventMask;
             unsigned char mask[(XI_LASTEVENT+7)/8] = {0};
@@ -86,29 +193,65 @@ class MouseHandler {
             XISelectEvents(display, DefaultRootWindow(display), &eventMask, 1);
             XFlush(display);
             XEvent event;
-            while (true) {
+            std::string saveString = "";
+            std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+            int eventCounter = 0;
+            while (eventCounter < 100) {
                 XNextEvent(display, &event);
                 if (event.xcookie.type == GenericEvent  && XGetEventData(display, &event.xcookie)) {                    
                     if (event.xcookie.evtype == XI_RawMotion) {
                         this->setMouseCoordinates();
-                        std::string message = "Mouse moved to (" + std::to_string(this->getX()) + "," + std::to_string(this->getY()) + ")\n";
-                        std::cout << message;
-                    } else if (event.xcookie.evtype == XI_RawButtonPress) {
-                        std::cout << "Mouse button pressed\n";
-                    } else if (event.xcookie.evtype == XI_RawButtonRelease) {
-                        std::cout << "Mouse button released\n";
-                    } else if (event.xcookie.evtype == XI_RawKeyPress) {
-                        std::cout << "Key pressed\n";
-                    } else if (event.xcookie.evtype == XI_RawKeyRelease) {
-                        XIRawEvent* rawEvent = (XIRawEvent*) event.xcookie.data;
-                        int keyCode = rawEvent->detail;
-                        std::cout << "Key " + std::to_string(keyCode) +" released\n";
+                        long long timeStamp = this->getTimestamp(start);
+                        saveString += std::to_string(event::MouseMotion) + ",(" + std::to_string(this->getX()) + "," + std::to_string(this->getY()) +")," + std::to_string(timeStamp) + "\n";
                     }
+                    else {
+                        event::EVENTTYPE eventType;
+                        if (event.xcookie.evtype == XI_RawButtonPress) {
+                            eventType = event::MousePressed;
+                        }
+                        else if (event.xcookie.evtype == XI_RawButtonRelease) {
+                            eventType = event::MouseReleased;
+                        } 
+                        else if (event.xcookie.evtype == XI_RawKeyPress) {
+                            eventType = event::KeyPressed;
+                        } 
+                        else {
+                            eventType = event::keyReleased;
+                        }
+                        XIRawEvent* rawEvent = (XIRawEvent *) event.xcookie.data;
+                        long long timeStamp = this->getTimestamp(start);
+                        int keyCode = rawEvent->detail;
+                        saveString += std::to_string(eventType) + "," + std::to_string(keyCode) + "," + std::to_string(timeStamp) + "\n";
+                    }
+                    eventCounter++;
                     XFreeEventData(display, &event.xcookie);
                 }
             }
+            this->saveEvent(saveString);
 
         } 
+
+        long long getTimestamp (std::chrono::time_point<std::chrono::system_clock> start) {
+            auto current = std::chrono::system_clock::now();
+            long long currentSinceEpoch = std::chrono::duration_cast<std::chrono::nanoseconds>(current.time_since_epoch()).count();
+            long long startSinceEpoch = std::chrono::duration_cast<std::chrono::nanoseconds>(start.time_since_epoch()).count();
+            long long durationSinceStart = currentSinceEpoch - startSinceEpoch;
+            return durationSinceStart;
+        }
+
+        void saveEvent (std::string saveString) {
+            std::ofstream file("test1.txt");
+            file << saveString;
+            file.close();
+            return;
+        }
+
+    private:
+
+        int x;
+        int y;
+        Display* display;
+        bool pressed;
 
 };
 
@@ -127,7 +270,7 @@ class Main {
 
 int main() {
     Display* display = XOpenDisplay(NULL);
-    MouseHandler mh(display);
-    mh.recordMouseEvent();
+    InputHandler ih(display);
+    ih.recordInputEvent();
     return 0;
 }
